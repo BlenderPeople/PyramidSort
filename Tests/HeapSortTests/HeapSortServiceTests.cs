5972a8a5eda22847c10e8dd19c0d8d3a09f9360a
whoami <whoami@MacBook-Air-whoami.local>
2026-01-16 14:07:41 +0700

    edit tests

--- Tests/HeapSortTests/HeapSortServiceTests.cs
+++ Tests/HeapSortTests/HeapSortServiceTests.cs
@@ -4,18 +4,18 @@ using NUnit.Framework;
 public class HeapSortServiceTests
 {
     [Test]
-    public void Sort_FullArray_SortsAscending()
+    public void Sort_UnsortedArray_SortsAscending()
     {
         var sorter = new HeapSortService();
-        var input = new[] { 3, 1, 2 };
+        var input = new[] { 3, 1, 4, 2 };
 
         var result = sorter.Sort(input, null, null);
 
-        Assert.That(result.SortedNumbers, Is.EqualTo(new[] { 1, 2, 3 }));
+        Assert.That(result.SortedNumbers, Is.EqualTo(new[] { 1, 2, 3, 4 }));
     }
 
     [Test]
-    public void Sort_RangeOnly_SortsOnlyThatSegment()
+    public void Sort_Range()
     {
         var sorter = new HeapSortService();
         var input = new[] { 5, 4, 3, 2, 1 };
@@ -24,5 +24,43 @@ public class HeapSortServiceTests
 
         Assert.That(result.SortedNumbers, Is.EqualTo(new[] { 5, 2, 3, 4, 1 }));
     }
-}
+ // null array - null&[], random array, pryamoy massiv, negativ array, 
+    [Test]
+    public void Sort_EmptyInvalidOperationException()
+    {
+        var sorter = new HeapSortService();
+        var input = Array.Empty<int>();
 
+        Assert.Throws<InvalidOperationException>(() => sorter.Sort(input, null, null));
+    }
+
+    [Test]
+    public void Sort_NullArrayArgumentNullException()
+    {
+        var sorter = new HeapSortService();
+
+        Assert.Throws<ArgumentNullException>(() => sorter.Sort(null!, null, null));
+    }
+
+    [Test]
+    public void Sort_PrymoiSortedArray_StaysSorted()
+    {
+        var sorter = new HeapSortService();
+        var input = new[] { 1, 2, 3, 4, 5 };
+
+        var result = sorter.Sort(input, null, null);
+
+        Assert.That(result.SortedNumbers, Is.EqualTo(new[] { 1, 2, 3, 4, 5 }));
+    }
+
+    [Test]
+    public void Sort_NegativeNumbers_SortsAscending()
+    {
+        var sorter = new HeapSortService();
+        var input = new[] { 3, -1, 2, -5 };
+
+        var result = sorter.Sort(input, null, null);
+
+        Assert.That(result.SortedNumbers, Is.EqualTo(new[] { -5, -1, 2, 3 }));
+    }
+}
